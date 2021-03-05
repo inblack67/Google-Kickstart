4a4ac9d94646d76d91799f1ae306312aee128265
Aman Bhardwaj <inblack>
2021-03-05 13:04:21 +0530

    how many houses can you buy => allocation

--- src/Allocation/index.ts
+++ src/Allocation/index.ts
@@ -1,33 +1,70 @@
 import readline from 'readline';
 
-const calculateMaxHouses = (houses: number, a: number, budget: number): number => {
+const calculateMaxHouses = (numberOfHouses: number, prices: number[], budget: number): number => {
     let max = 0;
-    let i = 1;
-    while (budget > 0 && houses > 0) {
-        const cost = a * i;
-        budget -= cost;
-        max++;
-        i++;
-        houses--;
+    const sortedPrices = [ ...prices ].sort((a, b) => a - b);
+    for (let i = 0; i < numberOfHouses; i++) {
+        const price = sortedPrices[ i ];
+        if (budget >= price) {
+            budget -= price;
+            max++;
+        }
     }
     return max;
 };
 
-export const maxHouses = () => {
+console.log(calculateMaxHouses(4, [ 20, 90, 40, 90 ], 100));
+console.log(calculateMaxHouses(4, [ 30, 30, 10, 10 ], 50));
+console.log(calculateMaxHouses(3, [ 999, 999, 999 ], 300));
+
+export const maxHouses = async () => {
     const myConsole = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    myConsole.question('Houses ? ', houses => {
-        myConsole.question('a ?', a => {
-            myConsole.question('budget ? ', budget => {
-                const result = calculateMaxHouses(+houses, +a, +budget);
-                console.log('result = ', result);
-                myConsole.close();
+    const q1 = (): Promise<number> => {
+        return new Promise((res) => {
+            myConsole.question('Number of test cases ? ', t => {
+                res(+t);
             });
         });
-    });
+    };
+    const q2 = (): Promise<number> => {
+        return new Promise((res) => {
+            myConsole.question('Number of houses ? ', n => {
+                res(+n);
+            });
+        });
+    };
+    const q3 = (): Promise<number> => {
+        return new Promise((res) => {
+            myConsole.question('Your budget ? ', b => {
+                res(+b);
+            });
+        });
+    };
+    const q4 = (): Promise<number> => {
+        return new Promise((res) => {
+            myConsole.question('Price ? ', p => {
+                res(+p);
+            });
+        });
+    };
+
+    const tests = await q1();
+    for (let i = 0; i < tests; i++) {
+        const numberOfHouses = await q2();
+        const budget = await q3();
+        const prices: number[] = [];
+        for (let i = 0; i < numberOfHouses; i++) {
+            const price = await q4();
+            prices.push(price);
+        }
+        myConsole.close();
+        const res = calculateMaxHouses(numberOfHouses, prices, budget);
+        console.log(`Case #${ i + 1 }: `, res);
+    }
 
     myConsole.on('close', () => {
         process.exit(0);
