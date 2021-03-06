66f56ed416896912b0756d4ff507f311652b0b2c
Aman Bhardwaj <inblack>
2021-03-06 12:11:39 +0530

    solved

--- src/Plates/index.ts
+++ src/Plates/index.ts
@@ -1,47 +1,54 @@
-const calculateSumOfElements = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
+const calculateSumOfElements2 = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
 
-const calculateMaxBeautifulPlates = (numberOfStacks: number, stackLength: number, maxPlates: number, stacks: number[][]): number => {
-    console.log(numberOfStacks);
-    console.log(stackLength);
-    console.log(maxPlates);
-    console.log(stacks);
+const getMaxBeauty = (arr1: number[], arr2: number[], maxPlates: number): number => {
 
     let res = 0;
+    let j = arr1.length;
 
-    for (let i = 0; i < stacks.length; i++) {
-        const stack = stacks[ i ];
-        const nextStack = stacks[ i + 1 ];
-        console.log(stack);
-
-        let j = stack.length;
-
-        while (j > 0) {
-            let newSet = [ ...stack.slice(0, j) ];
-            console.log(newSet);
-            for (let k = 0; k < stack.length; k++) {
-                console.log(j);
-                if (newSet.length === maxPlates) {
-                    const sum = calculateSumOfElements(newSet);
-                    console.log(sum);
-                    if (sum > res) {
-                        res = sum;
-                    }
-                } else {
-                    if (nextStack) {
-                        const restSet = nextStack.slice(0, maxPlates - newSet.length);
-                        newSet = [ ...newSet, ...restSet ];
-                        console.log(newSet);
-                    }
+    while (j > 0) {
+        let newSet = [ ...arr1.slice(0, j) ];
+        for (let k = 0; k < arr1.length; k++) {
+            if (newSet.length === maxPlates) {
+                const sum = calculateSumOfElements2(newSet);
+                if (sum > res) {
+                    res = sum;
                 }
+            } else {
+                const restSet = arr2.slice(0, maxPlates - newSet.length);
+                newSet = [ ...newSet, ...restSet ];
             }
-            j--;
         }
-
+        j--;
     }
 
     return res;
 };
 
-// console.log(calculateMaxBeautifulPlates(2, 4, 5, [ [ 10, 10, 100, 30 ], [ 80, 50, 10, 50 ] ]));
+const calculateMaxBeautifulPlates2 = (_: number, __: number, maxPlates: number, stacks: number[][]): number => {
+    let res = 0;
+    let count = 0;
 
-console.log(calculateMaxBeautifulPlates(3, 2, 3, [ [ 80, 80 ], [ 15, 50 ], [ 20, 10 ] ]));
+    for (let i = 0; i < stacks.length; i++) {
+        const stack = stacks[ i ];
+        const nextStack = stacks[ i + 1 ];
+        if (nextStack) {
+            const totalBeauty = getMaxBeauty(stack, nextStack, maxPlates);
+            if (totalBeauty > res) {
+                res = totalBeauty;
+            }
+        } else if (stacks.length > 2 && count === 0) {
+            const totalBeauty = getMaxBeauty(stack, stacks[ 0 ], maxPlates);
+            if (totalBeauty > res) {
+                res = totalBeauty;
+            }
+            count++;
+        } else {
+            break;
+        }
+    }
+    return res;
+};
+
+console.log(calculateMaxBeautifulPlates2(2, 4, 5, [ [ 10, 10, 100, 30 ], [ 80, 50, 10, 50 ] ]));
+
+console.log(calculateMaxBeautifulPlates2(3, 2, 3, [ [ 80, 80 ], [ 15, 50 ], [ 20, 10 ] ]));
--- src/Plates/submitted.js
+++ src/Plates/submitted.js
@@ -1,34 +1,59 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
 
-let currentLine = 0;
-const readLine = () => input[currentLine++];
+const calculateSumOfElements = (arr) => arr.reduce((a, b) => a + b, 0);
 
-const tests = readLine();
+const getMaxBeauty = (arr1, arr2, maxPlates) => {
+	let res = 0;
+	let j = arr1.length;
+
+	while (j > 0) {
+		let newSet = [ ...arr1.slice(0, j) ];
+		for (let k = 0; k < arr1.length; k++) {
+			if (newSet.length === maxPlates) {
+				const sum = calculateSumOfElements(newSet);
+				if (sum > res) {
+					res = sum;
+				}
+			} else {
+				const restSet = arr2.slice(0, maxPlates - newSet.length);
+				newSet = [ ...newSet, ...restSet ];
+			}
+		}
+		j--;
+	}
+
+	return res;
+};
 
 const calculateMaxBeautifulPlates = (numberOfStacks, stackLength, maxPlates, stacks) => {
 	let res = 0;
+	let count = 0;
+
 	for (let i = 0; i < stacks.length; i++) {
 		const stack = stacks[i];
-		const maxEl = Math.max(...stack);
-		const indexOfMaxEl = stack.indexOf(maxEl);
-		for (let j = 0; j < stack.length; j++) {
-			const el = stack[j];
-			if (j <= indexOfMaxEl && maxPlates > 0) {
-				res += el;
-				maxPlates--;
-			} else if (maxPlates > 0) {
-				if (i === stacks.length - 1) {
-					res += el;
-					maxPlates--;
-				}
-			} else {
-				break;
+		const nextStack = stacks[i + 1];
+		if (nextStack) {
+			const totalBeauty = getMaxBeauty(stack, nextStack, maxPlates);
+			if (totalBeauty > res) {
+				res = totalBeauty;
 			}
+		} else if (stacks.length > 2 && count === 0) {
+			const totalBeauty = getMaxBeauty(stack, stacks[0], maxPlates);
+			if (totalBeauty > res) {
+				res = totalBeauty;
+			}
+			count++;
+		} else {
+			break;
 		}
 	}
 	return res;
 };
+
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+let currentLine = 0;
+const readLine = () => input[currentLine++];
+const tests = readLine();
 
 for (let i = 0; i < +tests; i++) {
 	const [ numberOfStacks, stackLength, maxPlates ] = readLine().split(' ');
@@ -38,5 +63,5 @@ for (let i = 0; i < +tests; i++) {
 		stacks.push(beauties);
 	}
 	const res = calculateMaxBeautifulPlates(+numberOfStacks, +stackLength, +maxPlates, stacks);
-	console.log(`Case #${i}: ${res}`);
+	console.log(`Case #${i + 1}: ${res}`);
 }
