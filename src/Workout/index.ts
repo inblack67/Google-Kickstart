3ec9dc3a0d0ba01cf21bd3bade05077fb149eb28
Aman Bhardwaj <inblack>
2021-03-07 11:55:16 +0530

    workout init

--- src/Workout/index.ts
+++ src/Workout/index.ts
@@ -1,11 +1,26 @@
-const sessions = (n: number, m: number): number => {
-    // const minutes: number[] = [];
-    for (let i = 0; i < n; i++) {
-        console.log(i);
-        const exercisedFor = m * i; // minutes
-        console.log(exercisedFor);
+const calculateDifficulty = (a: number, b: number) => Math.abs(a - b);
+
+const sessions = (numberOfSessions: number, additionalSessions: number, minutes: number[]): number => {
+    console.log(numberOfSessions);
+    console.log(additionalSessions);
+    console.log(minutes);
+
+    const difficulties: number[] = [];
+
+    for (let i = 0; i < numberOfSessions; i++) {
+        const spentMinutes = minutes[ i ];
+        const nextSpentMinutes = minutes[ i + 1 ];
+        if (nextSpentMinutes) {
+            const difficulty = calculateDifficulty(spentMinutes, nextSpentMinutes);
+            difficulties.push(difficulty);
+        }
     }
+
+    const hardwork = Math.max(...difficulties);
+    console.log(hardwork);
+
     return 0;
 };
 
-console.log(sessions(5, 2));
+// n, k
+console.log(sessions(3, 1, [ 100, 200, 230 ]));
